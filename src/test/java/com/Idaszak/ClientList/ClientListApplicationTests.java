53643e9eb07ff5f6c8212770084e3ccc5c45a043
Okzsadi <grzesidaszak@gmail.com>
2025-01-21 22:55:23 +0100

    Initial Commit

---
+++ src/test/java/com/Idaszak/ClientList/ClientListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Idaszak.ClientList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
